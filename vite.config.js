4f9d76909e998b08235da0900ee6897269865b3d
jmailia <whatGravity@protonmail.com>
2024-07-09 09:25:38 -0700

    vite port fixes

--- vite.config.js
+++ vite.config.js
@@ -4,22 +4,15 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/",
-  plugins: [react()],
+  plugins: [
+    react(),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   },
-  preview: {
-    port: 3000,
-    strictPort: true,
-   },
   server: {
-    strictPort: true,
-    //host: true,
-    host: "0.0.0.0",
     port: 3000,
-    origin: "http://0.0.0.0:3000",
-  }
+    host: "0.0.0.0",  }
 })
