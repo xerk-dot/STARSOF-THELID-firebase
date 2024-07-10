5c041059dd6d435b439332016f4cf52471f2bb82
jmailia <whatGravity@protonmail.com>
2024-07-09 21:48:14 -0700

    footer fix

--- src/components/common/Footer.jsx
+++ src/components/common/Footer.jsx
@@ -18,7 +18,7 @@ const Footer = () => {
           <span>
             Developed by
             {' '}
-            <a href="https://github.com/jgudo">JULIUS GUEVARRA</a>
+            <a href="https://github.com/xerk-dot">Henry Hecht</a>
           </span>
         </strong>
       </div>
@@ -33,7 +33,7 @@ const Footer = () => {
         <strong>
           <span>
             Fork this project &nbsp;
-            <a href="https://github.com/jgudo/ecommerce-react">HERE</a>
+            <a href="https://github.com/xerk-dot/STARSOF-THELID-firebase">HERE</a>
           </span>
         </strong>
       </div>
