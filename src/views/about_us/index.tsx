d90d6be2ac310b58e679144e75eb2b59c7087523
jmailia <whatGravity@protonmail.com>
2024-07-10 22:39:21 -0700

    parallax mid implementation

--- src/views/about_us/index.tsx
+++ src/views/about_us/index.tsx
@@ -1,87 +1,91 @@
-// this comes from the framer-motion docs, just ported to twcss
-import { wrap } from "@motionone/utils";
-import React from "react";
-
+import { useDocumentTitle, useScrollTop } from '../../hooks';
+import React from 'react';
+import "./styles.css";
+import { useRef } from "react";
 import {
   motion,
-  AnimatePresence,
   useScroll,
   useSpring,
   useTransform,
   useMotionValue,
   useVelocity,
-  useAnimationFrame,
+  useAnimationFrame
 } from "framer-motion";
+import { wrap } from "@motionone/utils";
 
 interface ParallaxProps {
   children: string;
   baseVelocity: number;
 }
 
-export function VelocityScroll() {
-  function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
-    const baseX = useMotionValue(0);
-    const { scrollY } = useScroll();
-    const scrollVelocity = useVelocity(scrollY);
-    const smoothVelocity = useSpring(scrollVelocity, {
-      damping: 50,
-      stiffness: 400,
-    });
-    const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
-      clamp: false,
-    });
+
+function ParallaxText({ children, baseVelocity = 100 }: ParallaxProps) {
+  const baseX = useMotionValue(0);
+  const { scrollY } = useScroll();
+  const scrollVelocity = useVelocity(scrollY);
+  const smoothVelocity = useSpring(scrollVelocity, {
+    damping: 50,
+    stiffness: 400
+  });
+  const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
+    clamp: false
+  });
+
+  /**
+   * This is a magic wrapping for the length of the text - you
+   * have to replace for wrapping that works for you or dynamically
+   * calculate
+   */
+  const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
+  const directionFactor = useRef<number>(1);
+  useAnimationFrame((t, delta) => {
+    let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
 
     /**
-     * This is a magic wrapping for the length of the text - you
-     * have to replace for wrapping that works for you or dynamically
-     * calculate
+     * This is what changes the direction of the scroll once we
+     * switch scrolling directions.
      */
-    const x = useTransform(baseX, (v) => $ {wrap(-20, -45, v)}%);
+    if (velocityFactor.get() < 0) {
+      directionFactor.current = -1;
+    } else if (velocityFactor.get() > 0) {
+      directionFactor.current = 1;
+    }
 
-    const directionFactor = React.useRef<number>(1);
-    useAnimationFrame((t, delta) => {
-      let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
+    moveBy += directionFactor.current * moveBy * velocityFactor.get();
 
-      /**
-       * This is what changes the direction of the scroll once we
-       * switch scrolling directions.
-       */
-      if (velocityFactor.get() < 0) {
-        directionFactor.current = -1;
-      } else if (velocityFactor.get() > 0) {
-        directionFactor.current = 1;
-      }
+    baseX.set(baseX.get() + moveBy);
+  });
 
-      moveBy += directionFactor.current * moveBy * velocityFactor.get();
-
-      baseX.set(baseX.get() + moveBy);
-    });
-
-    /**
-     * The number of times to repeat the child text should be dynamically calculated
-     * based on the size of the text and viewport. Likewise, the x motion value is
-     * currently wrapped between -20 and -45% - this 25% is derived from the fact
-     * we have four children (100% / 4). This would also want deriving from the
-     * dynamically generated number of children.
-     */
-    return (
-      <div className="parallax">
-        <motion.div
-          className="scroller text-center font-display text-4xl font-bold tracking-[-0.02em] drop-shadow-sm md:text-7xl md:leading-[5rem]"
-          style={{ x }}
-        >
-          <span>{children} </span>
-          <span>{children} </span>
-          <span>{children} </span>
-          <span>{children} </span>
-        </motion.div>
-      </div>
-    );
-  }
+  /**
+   * The number of times to repeat the child text should be dynamically calculated
+   * based on the size of the text and viewport. Likewise, the x motion value is
+   * currently wrapped between -20 and -45% - this 25% is derived from the fact
+   * we have four children (100% / 4). This would also want deriving from the
+   * dynamically generated number of children.
+   */
   return (
-    <section>
-      <ParallaxText baseVelocity={5}>Variant Vault</ParallaxText>
-      <ParallaxText baseVelocity={-5}>Variant Vault</ParallaxText>
-    </section>
+    <div className="parallax">
+      <motion.div className="scroller" style={{ x }}>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+        <span>{children} </span>
+      </motion.div>
+    </div>
   );
 }
+
+
+const AboutUs = () => {
+  useDocumentTitle('About Us | Starsof');
+  useScrollTop();
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default AboutUs;
